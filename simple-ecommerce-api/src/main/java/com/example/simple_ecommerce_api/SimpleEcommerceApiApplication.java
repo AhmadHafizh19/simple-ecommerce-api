96c062806ab63c4fb7e16608c07208e6885719bb
Ahmad Hafizh Assaad <71488@fif.co.id>
2025-06-12 13:54:54 +0700

    :tada: ready to challenge

---
+++ simple-ecommerce-api/src/main/java/com/example/simple_ecommerce_api/SimpleEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_ecommerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEcommerceApiApplication.class, args);
+	}
+
+}
